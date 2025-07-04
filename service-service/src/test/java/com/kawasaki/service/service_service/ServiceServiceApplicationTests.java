25501c952394b24c4396f828a9b3b7e1529372bc
Haonan Xu <xhn20001106@gmail.com>
2025-07-04 02:13:30 -0400

    initial project setup

---
+++ service-service/src/test/java/com/kawasaki/service/service_service/ServiceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kawasaki.service.service_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
